fc09acf7f71331c2dd2f918164ac868e1c63a179
Javier Fraga <javier.s.fraga@gmail.com>
2023-09-09 05:44:15 -0700

    remove functions file, not used

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
 -- reload init.lua :luafile %
 require "javier.options"
-require "javier.functions"
 require "javier.whitespace"
 require "javier.cursor-position"
 require "javier.search-jumps"
 require "javier.buffer-reload"
 require "javier.diff-unsaved"
 require "javier.file-dir-path"
+require "javier.quickfix-remap"
