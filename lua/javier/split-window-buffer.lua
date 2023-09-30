bc1d2d9941a99768aeaa5dbac15165e7769c7fd5
Javier Fraga <javier.s.fraga@gmail.com>
2023-09-29 19:21:39 -0700

    Add file

---
+++ lua/javier/split-window-buffer.lua
@@ -0,0 +1,16 @@
+-----------------------------------------
+-- Cheatsheet for window/buffer splitting
+-----------------------------------------
+
+-- " window split, full window split
+-- nmap <leader>swh  :topleft  vsplit<CR>
+-- nmap <leader>swl :botright vsplit<CR>
+-- nmap <leader>swk    :topleft  split<CR>
+-- nmap <leader>swj  :botright split<CR>
+
+-- " buffer split, only split within buffer
+-- nmap <leader>sh   :leftabove  vsplit<CR>
+-- nmap <leader>sl  :rightbelow vsplit<CR>
+-- nmap <leader>sk     :leftabove  split<CR>
+-- nmap <leader>sj   :rightbelow split<CR>
+
