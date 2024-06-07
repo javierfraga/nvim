d35f8a46568e8c9e2c50dad93b36d20be990632e
Javier Fraga <javier.s.fraga@gmail.com>
2024-06-07 12:15:35 -0700

    Add full path to dest for git clone

--- pack/nvim-plugins/start/run-me.sh
+++ pack/nvim-plugins/start/run-me.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-git clone https://github.com/christoomey/vim-tmux-navigator.git
-git clone https://github.com/junegunn/vim-easy-align.git
-git clone https://github.com/numToStr/Comment.nvim.git
+git clone https://github.com/christoomey/vim-tmux-navigator.git  ~/.config/nvim/pack/nvim-plugins/startvim-tmux-navigator
+git clone https://github.com/junegunn/vim-easy-align.git         ~/.config/nvim/pack/nvim-plugins/startvim-easy-align
+git clone https://github.com/numToStr/Comment.nvim.git           ~/.config/nvim/pack/nvim-plugins/startComment.nvim
