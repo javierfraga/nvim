7ee2aadf7370c5b3a1daae2249b1e245adb6413a
Javier Fraga <javier.s.fraga@gmail.com>
2024-06-07 13:01:30 -0700

    Fix typo

--- pack/nvim-plugins/start/run-me.sh
+++ pack/nvim-plugins/start/run-me.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/christoomey/vim-tmux-navigator.git  "$HOME/.config/nvim/pack/nvim-plugins/startvim-tmux-navigator"
-git clone https://github.com/junegunn/vim-easy-align.git         "$HOME/.config/nvim/pack/nvim-plugins/startvim-easy-align"
-git clone https://github.com/numToStr/Comment.nvim.git           "$HOME/.config/nvim/pack/nvim-plugins/startComment.nvim"
+git clone https://github.com/christoomey/vim-tmux-navigator.git  "$HOME/.config/nvim/pack/nvim-plugins/start/vim-tmux-navigator"
+git clone https://github.com/junegunn/vim-easy-align.git         "$HOME/.config/nvim/pack/nvim-plugins/start/vim-easy-align"
+git clone https://github.com/numToStr/Comment.nvim.git           "$HOME/.config/nvim/pack/nvim-plugins/start/Comment.nvim"
