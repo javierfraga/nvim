b9ed4ccf9d44dd5285e829fbe36af1e8d7d59252
Javier Fraga <javier.s.fraga@gmail.com>
2024-06-07 12:59:23 -0700

    Add /Users/javier and change to bash

--- pack/nvim-plugins/start/run-me.sh
+++ pack/nvim-plugins/start/run-me.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
+#!/bin/bash
 
-git clone https://github.com/christoomey/vim-tmux-navigator.git  ~/.config/nvim/pack/nvim-plugins/startvim-tmux-navigator
-git clone https://github.com/junegunn/vim-easy-align.git         ~/.config/nvim/pack/nvim-plugins/startvim-easy-align
-git clone https://github.com/numToStr/Comment.nvim.git           ~/.config/nvim/pack/nvim-plugins/startComment.nvim
+git clone https://github.com/christoomey/vim-tmux-navigator.git  "$HOME/.config/nvim/pack/nvim-plugins/startvim-tmux-navigator"
+git clone https://github.com/junegunn/vim-easy-align.git         "$HOME/.config/nvim/pack/nvim-plugins/startvim-easy-align"
+git clone https://github.com/numToStr/Comment.nvim.git           "$HOME/.config/nvim/pack/nvim-plugins/startComment.nvim"
