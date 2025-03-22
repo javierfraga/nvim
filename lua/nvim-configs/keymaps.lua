d8acc61cb699638820eeb80a78a0327d62e76608
Javier Fraga <javier.s.fraga@gmail.com>
2025-03-21 21:58:54 -0500

    Finally clean up keymaps

--- lua/nvim-configs/keymaps.lua
+++ lua/nvim-configs/keymaps.lua
@@ -24,106 +24,102 @@ vim.g.maplocalleader = " "
 --   term_mode = "t",
 --   command_mode = "c",
 
--- Navigate buffers
--- keymap("n", "<Tab>", ":bnext<CR>", opts)
--- keymap("n", "<C-i>", ":bnext<CR>", opts)
--- keymap("n", "<S-Tab>", ":bprevious<CR>", opts)
--- keymap("n", "<C-i>", ":echo 'Ctrl-Tab pressed'<CR>", opts)
--- keymap("n", "<C-Tab>", ":echo 'Ctrl-Tab pressed'<CR>", opts)
--- keymap("n", "<C-S-Tab>", ":echo 'Ctrl-Shift-Tab pressed'<CR>", opts)
-keymap("n",   "<C-Tab>", ":bnext<CR>",                        opts )
-keymap("n", "<C-S-Tab>", ":bprevious<CR>",                    opts )
-keymap("n", "<A-S-Tab>", ":echo 'Alt-Shift-Tab pressed'<CR>", opts )
-keymap("n",   "<A-Tab>", ":echo 'Alt-Tab pressed'<CR>",       opts )
--- Open new buffer
--- keymap("n", "<C-t>", ":enew<CR>", opts)
--- keymap("i", "<C-t>", "<ESC>:enew<CR>", opts)
--- Split buffers
--- keymap("n", "<C-h>", ":echo 'Ctrl-h pressed'<CR>", opts)
--- keymap("n", "<C-j>", ":echo 'Ctrl-j pressed'<CR>", opts)
--- keymap("n", "<C-k>", ":echo 'Ctrl-k pressed'<CR>", opts)
--- keymap("n", "<C-l>", ":echo 'Ctrl-l pressed'<CR>", opts)
+------------------------------------------------------
+--  Testing Keybindings, overwrite when necessary   --
+------------------------------------------------------
+-- Navigate buffers Testing
+keymap("n", "<C-Tab>", ":echo 'Ctrl-Tab pressed'<CR>", opts)
+keymap("n", "<C-S-Tab>", ":echo 'Ctrl-Shift-Tab pressed'<CR>", opts)
+keymap("n", "<C-h>", ":echo 'Ctrl-h pressed'<CR>", opts)
+keymap("n", "<C-j>", ":echo 'Ctrl-j pressed'<CR>", opts)
+keymap("n", "<C-k>", ":echo 'Ctrl-k pressed'<CR>", opts)
+keymap("n", "<C-l>", ":echo 'Ctrl-l pressed'<CR>", opts)
+keymap("n", "<C-H>", ":echo 'Ctrl-h pressed'<CR>", opts)
+keymap("n", "<C-J>", ":echo 'Ctrl-j pressed'<CR>", opts)
+keymap("n", "<C-K>", ":echo 'Ctrl-k pressed'<CR>", opts)
+keymap("n", "<C-L>", ":echo 'Ctrl-l pressed'<CR>", opts)
 keymap("n", "<C-S-h>", ":echo 'Ctrl-Shift-h pressed'<CR>", opts)
 keymap("n", "<C-S-j>", ":echo 'Ctrl-Shift-j pressed'<CR>", opts)
 keymap("n", "<C-S-k>", ":echo 'Ctrl-Shift-k pressed'<CR>", opts)
 keymap("n", "<C-S-l>", ":echo 'Ctrl-Shift-l pressed'<CR>", opts)
--- keymap("n", "<C-l>", "<C-w>v", opts)
--- keymap("n", "<C-K>", ":aboveleft split<CR>", opts)
--- keymap("n", "<C-J>", "<C-w>s", opts)
--- keymap("n", "<C-H>", ":leftabove vsplit<CR>", opts)
--- Close current buffer
--- keymap("n", "<C-w>", ":bd<CR>", opts)
--- Open last close buffer
--- keymap("n", "<C-T>", ":e#<CR>", opts)
-
--- keymap("n", "<C-L>", ":bnext<CR>", opts)
--- keymap("n", "<C-H>", ":bprevious<CR>", opts)
--- keymap("n", "<leader>l", ":bnext<CR>", opts)
--- keymap("n", "<leader>h", ":bprevious<CR>", opts)
-
--- Normal --
--- Better window navigation
--- keymap("n", "<C-h>", "<C-w>h", opts)
--- for whatever reason, this is not working in Macos
--- and creating conflicts for below mappings
--- keymap("n", "<S-C-h>", "<C-w>h", opts)
--- keymap("n", "<S-C-j>", "<C-w>j", opts)
--- keymap("n", "<S-C-k>", "<C-w>k", opts)
--- keymap("n", "<S-C-l>", "<C-w>l", opts)
-
--- this is a good one. Just memorize
--- keymap("n", "<leader>e", ":Lex 30<cr>", opts)
-
--- Resize with arrows
--- for whatever reason, this is not working in Macos
--- keymap("n", "<A-S-Up>", ":resize +2<CR>", opts)
--- keymap("n", "<A-S-Down>", ":resize -2<CR>", opts)
--- keymap("n", "<A-S-Left>", ":vertical resize -2<CR>", opts)
--- keymap("n", "<A-S-Right>", ":vertical resize +2<CR>", opts)
-keymap("n", "<A-h>", ":echo 'Alt+h'<CR>", opts)
-keymap("n", "<A-j>", ":echo 'Alt+j'<CR>", opts)
-keymap("n", "<A-k>", ":echo 'Alt+k'<CR>", opts)
-keymap("n", "<A-l>", ":echo 'Alt+l'<CR>", opts)
+keymap("n", "<A-Tab>", ":echo 'Alt-Tab pressed'<CR>", opts)
+keymap("n", "<A-S-Tab>", ":echo 'Alt-Shift-Tab pressed'<CR>", opts)
+keymap("n", "<A-h>", ":echo 'Alt-h pressed'<CR>", opts)
+keymap("n", "<A-j>", ":echo 'Alt-j pressed'<CR>", opts)
+keymap("n", "<A-k>", ":echo 'Alt-k pressed'<CR>", opts)
+keymap("n", "<A-l>", ":echo 'Alt-l pressed'<CR>", opts)
 keymap("n", "<A-H>", ":echo 'Alt+Shift+h'<CR>", opts)
 keymap("n", "<A-J>", ":echo 'Alt+Shift+j'<CR>", opts)
 keymap("n", "<A-K>", ":echo 'Alt+Shift+k'<CR>", opts)
 keymap("n", "<A-L>", ":echo 'Alt+Shift+l'<CR>", opts)
+-- Open Close buffers
+keymap('n', '<C-t>', ':echo "Ctrl-t was pressed"<CR>', opts)
+keymap('n', '<C-S-t>', ':echo "Ctrl-Shift-t was pressed"<CR>', opts)
+keymap('n', '<A-w>', ':echo "Alt-w was pressed"<CR>', opts)
+keymap('n', '<A-b>', ':echo "Alt-b was pressed"<CR>', opts)
+keymap('n', '<A-m>', ':echo "Alt-m was pressed"<CR>', opts)
+
+-- Resize with arrows
+keymap("n", "<A-Up>", ":echo 'Alt-Up was pressed'<CR>", opts)
+keymap("n", "<A-Down>", ":echo 'Alt-Down was pressed'<CR>", opts)
+keymap("n", "<A-Left>", ":echo 'Alt-Left was pressed'<CR>", opts)
+keymap("n", "<A-Right>", ":echo 'Alt-Right was pressed'<CR>", opts)
+-- Insert --
+-- Press jk fast to enter
+keymap("i", "jk", ":echo 'jk was pressed'<CR>", opts)
+keymap("i", "kj", ":echo 'kj was pressed'<CR>", opts)
+keymap("i", "jj", ":echo 'jj was pressed'<CR>", opts)
+
+
+-----------------------------------------------------
+--               Desired Keybindings              ---
+-----------------------------------------------------
+-- Navigate buffers Desired Settings, try here, but copy to local file (see below):
+keymap("n", "<C-Tab>", ":bnext<CR>", opts)
+keymap("n", "<C-S-Tab>", ":bprevious<CR>", opts)
+-- keymap("n", "<C-h>", "<Ctrl-w>h", opts)
+-- keymap("n", "<C-j>", "<Ctrl-w>j", opts)
+-- keymap("n", "<C-k>", "<Ctrl-w>k", opts)
+-- keymap("n", "<C-l>", "<Ctrl-w>l", opts)
+keymap("n", "<C-S-h>", ":topleft vsplit<CR>", opts)
+keymap("n", "<C-S-j>", "<C-w>s", opts)
+keymap("n", "<C-S-k>", ":aboveleft split<CR>", opts)
+keymap("n", "<C-S-l>", "<C-w>v", opts)
+-- keymap("n", "<A-Tab>", ":echo 'Alt-Tab pressed'<CR>", opts)
+-- keymap("n", "<A-S-Tab>", ":echo 'Alt-Shift-Tab pressed'<CR>", opts)
+-- Move to beginning of line with <Alt-h>
+keymap('n', '<A-h>', '0', opts)
+-- keymap("n", "<A-j>", ":echo 'Alt-j pressed'<CR>", opts)
+-- keymap("n", "<A-k>", ":echo 'Alt-k pressed'<CR>", opts)
+-- Move to end of line with <Alt-l>
+keymap('n', '<A-l>', '$', opts)
+-- keymap("n", "<A-S-h>", ":echo 'Alt-Shift-h pressed'<CR>", opts)
+-- keymap("n", "<A-S-j>", ":echo 'Alt-Shift-j pressed'<CR>", opts)
+-- keymap("n", "<A-S-k>", ":echo 'Alt-Shift-k pressed'<CR>", opts)
+-- keymap("n", "<A-S-l>", ":echo 'Alt-Shift-l pressed'<CR>", opts)
+-- Open Close buffers
+keymap('n', '<C-t>', ':enew<CR>', opts)
+keymap('n', '<C-S-t>', ':e#<CR>', opts)
+keymap('n', '<A-w>', ':x<CR>', opts)
+keymap('n', '<A-b>', ':bd<CR>', opts)
+-- Move cursor to the middle character of the line with <Alt-m>
+vim.keymap.set('n', '<A-m>', function()
+  local line = vim.api.nvim_get_current_line()
+  local mid = math.floor(#line / 2)
+  vim.api.nvim_win_set_cursor(0, { vim.api.nvim_win_get_cursor(0)[1], mid })
+end, opts)
+-- Resize with arrows
 keymap("n", "<A-Up>", ":resize +2<CR>", opts)
 keymap("n", "<A-Down>", ":resize -2<CR>", opts)
 keymap("n", "<A-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<A-Right>", ":vertical resize +2<CR>", opts)
-
-
 -- Insert --
 -- Press jk fast to enter
 keymap("i", "jk", "<ESC>", opts)
 keymap("i", "kj", "<ESC>", opts)
 keymap("i", "jj", "<ESC>", opts)
 
--- Visual --
--- Stay in indent mode
--- keymap("v", "<A-h>", "<gv", opts)
--- keymap("v", "<A-l>", ">gv", opts)
 
--- Move text up and down
--- keymap("v", "<A-j>", ":m .+1<CR>==", opts)
--- keymap("v", "<A-k>", ":m .-2<CR>==", opts)
 
 -- Copy and Paste, Paste string remain in register
 keymap("v", "p", '"_dP', opts)
-
--- Visual Block --
--- Move text up and down
--- keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
--- keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
-
--- Terminal --
--- Better terminal navigation
--- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
--- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
--- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
--- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
--- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
--- keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
--- keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
